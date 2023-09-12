71e501108863473f8defede739653e72254d4ef0
LimboFPS <tbosley100@gmail.com>
2023-09-12 18:53:00 -0400

    Changed to IPostDBLoadMod, imported modConfig

--- ServerMod/src/mod.ts
+++ ServerMod/src/mod.ts
@@ -1,20 +1,12 @@
 import { DependencyContainer } from "tsyringe";
 
-import { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod";
+import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
 import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+import modConfig from "cfg/config.json";
 
-class Mod implements IPreAkiLoadMod
+class Mod implements IPostDBLoadMod
 {
-    private modConfig = require("../cfg/config.json");
-
-    // Code added here will load BEFORE the server has started loading
-    public preAkiLoad(container: DependencyContainer): void 
-    {
-        const logger = container.resolve<ILogger>("WinstonLogger");
-        logger.info("Loading: ClearVision");
-    }
-
     // Code added here will be run AFTER the server has started
     public postDBLoad(container: DependencyContainer): void 
     {
@@ -22,8 +14,6 @@ class Mod implements IPreAkiLoadMod
         const logger = container.resolve<ILogger>("WinstonLogger");
         try 
         {
-            logger.info("CV: Applying tune ups!");
-
             const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
             const items = databaseServer.getTables().templates.items;
 
@@ -33,78 +23,79 @@ class Mod implements IPreAkiLoadMod
             const pnv10 = items["5c0696830db834001d23f5da"];
             const t7 = items["5c110624d174af029e69734c"];
 
-            if (this.modConfig.ClearVision.Enabled) 
+            if (modConfig.ClearVision.Enabled) 
             {
-                if (this.modConfig.PVS14.Enabled) 
+                //AN/PVS-14 Night Vision Monocular
+                if (modConfig.PVS14.Enabled) 
                 {
-                    pvs14._props.Intensity = this.modConfig.PVS14.Intensity;
-                    pvs14._props.Mask = this.modConfig.PVS14.Mask;
-                    pvs14._props.MaskSize = this.modConfig.PVS14.MaskSize;
-                    pvs14._props.NoiseIntensity = this.modConfig.PVS14.NoiseIntensity;
-                    pvs14._props.NoiseScale = this.modConfig.PVS14.NoiseScale;
-                    pvs14._props.DiffuseIntensity = this.modConfig.PVS14.DiffuseIntensity;
-                    pvs14._props.Color.r = this.modConfig.PVS14.Red;
-                    pvs14._props.Color.g = this.modConfig.PVS14.Green;
-                    pvs14._props.Color.b = this.modConfig.PVS14.Blue;
-                    pvs14._props.Color.a = this.modConfig.PVS14.Alpha;
+                    pvs14._props.Intensity = modConfig.PVS14.Intensity;
+                    pvs14._props.Mask = modConfig.PVS14.Mask;
+                    pvs14._props.MaskSize = modConfig.PVS14.MaskSize;
+                    pvs14._props.NoiseIntensity = modConfig.PVS14.NoiseIntensity;
+                    pvs14._props.NoiseScale = modConfig.PVS14.NoiseScale;
+                    pvs14._props.DiffuseIntensity = modConfig.PVS14.DiffuseIntensity;
+                    pvs14._props.Color.r = modConfig.PVS14.Red;
+                    pvs14._props.Color.g = modConfig.PVS14.Green;
+                    pvs14._props.Color.b = modConfig.PVS14.Blue;
+                    pvs14._props.Color.a = modConfig.PVS14.Alpha;
                     logger.info("CV: AN/PVS14 Monocular tuned!");
                 }
 
                 //Armasight N-15
-                if (this.modConfig.N15.Enabled) 
+                if (modConfig.N15.Enabled) 
                 {
-                    n15._props.Intensity = this.modConfig.N15.Intensity;
-                    n15._props.Mask = this.modConfig.N15.Mask;
-                    n15._props.MaskSize = this.modConfig.N15.MaskSize;
-                    n15._props.NoiseIntensity = this.modConfig.N15.NoiseIntensity;
-                    n15._props.NoiseScale = this.modConfig.N15.NoiseScale;
-                    n15._props.DiffuseIntensity = this.modConfig.N15.DiffuseIntensity;
-                    n15._props.Color.r = this.modConfig.N15.Red;
-                    n15._props.Color.g = this.modConfig.N15.Green;
-                    n15._props.Color.b = this.modConfig.N15.Blue;
-                    n15._props.Color.a = this.modConfig.N15.Alpha;
+                    n15._props.Intensity = modConfig.N15.Intensity;
+                    n15._props.Mask = modConfig.N15.Mask;
+                    n15._props.MaskSize = modConfig.N15.MaskSize;
+                    n15._props.NoiseIntensity = modConfig.N15.NoiseIntensity;
+                    n15._props.NoiseScale = modConfig.N15.NoiseScale;
+                    n15._props.DiffuseIntensity = modConfig.N15.DiffuseIntensity;
+                    n15._props.Color.r = modConfig.N15.Red;
+                    n15._props.Color.g = modConfig.N15.Green;
+                    n15._props.Color.b = modConfig.N15.Blue;
+                    n15._props.Color.a = modConfig.N15.Alpha;
                     logger.info("CV: Armasight N-15 tuned!");
                 }
                 //GPNVG-18
-                if (this.modConfig.GPNVG.Enabled) 
+                if (modConfig.GPNVG.Enabled) 
                 {
-                    gpnvg._props.Intensity = this.modConfig.GPNVG.Intensity;
-                    gpnvg._props.Mask = this.modConfig.GPNVG.Mask;
-                    gpnvg._props.MaskSize = this.modConfig.GPNVG.MaskSize;
-                    gpnvg._props.NoiseIntensity = this.modConfig.GPNVG.NoiseIntensity;
-                    gpnvg._props.NoiseScale = this.modConfig.GPNVG.NoiseScale;
-                    gpnvg._props.DiffuseIntensity = this.modConfig.GPNVG.DiffuseIntensity;
-                    gpnvg._props.Color.r = this.modConfig.GPNVG.Red;
-                    gpnvg._props.Color.g = this.modConfig.GPNVG.Green;
-                    gpnvg._props.Color.b = this.modConfig.GPNVG.Blue;
-                    gpnvg._props.Color.a = this.modConfig.GPNVG.Alpha;
+                    gpnvg._props.Intensity = modConfig.GPNVG.Intensity;
+                    gpnvg._props.Mask = modConfig.GPNVG.Mask;
+                    gpnvg._props.MaskSize = modConfig.GPNVG.MaskSize;
+                    gpnvg._props.NoiseIntensity = modConfig.GPNVG.NoiseIntensity;
+                    gpnvg._props.NoiseScale = modConfig.GPNVG.NoiseScale;
+                    gpnvg._props.DiffuseIntensity = modConfig.GPNVG.DiffuseIntensity;
+                    gpnvg._props.Color.r = modConfig.GPNVG.Red;
+                    gpnvg._props.Color.g = modConfig.GPNVG.Green;
+                    gpnvg._props.Color.b = modConfig.GPNVG.Blue;
+                    gpnvg._props.Color.a = modConfig.GPNVG.Alpha;
                     logger.info("CV: GPNVG-18 tuned!");
                 }
                 //PNV-10T
-                if (this.modConfig.PNV10T.Enabled) 
+                if (modConfig.PNV10T.Enabled) 
                 {
-                    pnv10._props.Intensity = this.modConfig.PNV10T.Intensity;
-                    pnv10._props.Mask = this.modConfig.PNV10T.Mask;
-                    pnv10._props.MaskSize = this.modConfig.PNV10T.MaskSize;
-                    pnv10._props.NoiseIntensity = this.modConfig.PNV10T.NoiseIntensity;
-                    pnv10._props.NoiseScale = this.modConfig.PNV10T.NoiseScale;
-                    pnv10._props.DiffuseIntensity = this.modConfig.PNV10T.DiffuseIntensity;
-                    pnv10._props.Color.r = this.modConfig.PNV10T.Red;
-                    pnv10._props.Color.g = this.modConfig.PNV10T.Green;
-                    pnv10._props.Color.b = this.modConfig.PNV10T.Blue;
-                    pnv10._props.Color.a = this.modConfig.PNV10T.Alpha;
+                    pnv10._props.Intensity = modConfig.PNV10T.Intensity;
+                    pnv10._props.Mask = modConfig.PNV10T.Mask;
+                    pnv10._props.MaskSize = modConfig.PNV10T.MaskSize;
+                    pnv10._props.NoiseIntensity = modConfig.PNV10T.NoiseIntensity;
+                    pnv10._props.NoiseScale = modConfig.PNV10T.NoiseScale;
+                    pnv10._props.DiffuseIntensity = modConfig.PNV10T.DiffuseIntensity;
+                    pnv10._props.Color.r = modConfig.PNV10T.Red;
+                    pnv10._props.Color.g = modConfig.PNV10T.Green;
+                    pnv10._props.Color.b = modConfig.PNV10T.Blue;
+                    pnv10._props.Color.a = modConfig.PNV10T.Alpha;
                     logger.info("CV: PNV-10T tuned!");
                 }
                 //T-7 Thermal Goggles
-                if (this.modConfig.T7.Enabled) 
+                if (modConfig.T7.Enabled) 
                 {
-                    t7._props.IsNoisy = this.modConfig.T7.Noise;
-                    t7._props.NoiseIntensity = this.modConfig.T7.NoiseIntensity;
-                    t7._props.IsMotionBlurred = this.modConfig.T7.MotionBlur;
-                    t7._props.Mask = this.modConfig.T7.Mask;
-                    t7._props.MaskSize = this.modConfig.T7.MaskSize;
-                    t7._props.HeatMin = this.modConfig.T7.HeatMin;
-                    t7._props.ColdMax = this.modConfig.T7.ColdMax;
+                    t7._props.IsNoisy = modConfig.T7.Noise;
+                    t7._props.NoiseIntensity = modConfig.T7.NoiseIntensity;
+                    t7._props.IsMotionBlurred = modConfig.T7.MotionBlur;
+                    t7._props.Mask = modConfig.T7.Mask;
+                    t7._props.MaskSize = modConfig.T7.MaskSize;
+                    t7._props.HeatMin = modConfig.T7.HeatMin;
+                    t7._props.ColdMax = modConfig.T7.ColdMax;
                     logger.info("CV: T-7 Thermal Goggles tuned!");
                 }
             }

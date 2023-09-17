b13ae1a87db87e745533dcb54b6a6a3deab9d3b6
LimboFPS <tbosley100@gmail.com>
2023-09-17 14:51:05 -0400

    hotfix for null errors and bad if statements

--- ClientMod/ClearVisionPatches.cs
+++ ClientMod/ClearVisionPatches.cs
@@ -17,7 +17,7 @@ namespace ClearVision {
                 GameObject maincam = GameObject.Find("FPS Camera");
                 NightVision nv = maincam.GetComponent<NightVision>();
                 CC_Vintage vintage = maincam.GetComponent<CC_Vintage>();
-                if(nv.On || nv.InProcessSwitching) {
+                if(nv != null && (nv.On || nv.InProcessSwitching)) {
                     nv.Intensity = Plugin.NVGIntensity.Value;
                     nv.NoiseIntensity = Plugin.NVGNoiseIntensity.Value;
                     nv.NoiseScale = Plugin.NVGNoiseScale.Value;
@@ -57,24 +57,34 @@ namespace ClearVision {
                 GameObject maincam = GameObject.Find("FPS Camera");
                 ThermalVision t7 = maincam.GetComponent<ThermalVision>();
                 CC_Vintage vintage = maincam.GetComponent<CC_Vintage>();
-                // Get BaseOpticScope(Clone) obj
+                // Get BaseOpticScope(Clone) obj, but need to check if it actually exists
                 GameObject scope = GameObject.FindGameObjectWithTag("OpticCamera");
                 ThermalVision thermal = scope.GetComponent<ThermalVision>();
 
-                if(t7.On) {
+                if(t7 != null && (t7.On || t7.InProcessSwitching)) {
                     t7.IsGlitch = Plugin.T7Glitch.Value;
                     t7.IsPixelated = Plugin.T7Pixel.Value;
                     t7.IsNoisy = Plugin.T7Glitch.Value;
                     t7.IsFpsStuck = Plugin.T7Glitch.Value;
-                    if (Plugin.T7Glitch.Value) {
+                    if (Plugin.T7Fps.Value) {
                         t7.StuckFpsUtilities.MaxFramerate = Plugin.T7FpsMax.Value;
                         t7.StuckFpsUtilities.MinFramerate = Plugin.T7FpsMax.Value;
                     }
-                    t7.TextureMask.Size = Plugin.T7MaskSize.Value;
+                    if(Plugin.T7MaskEnabled.Value) {
+                        t7.TextureMask.enabled = true;
+                        t7.TextureMask.Size = Plugin.T7MaskSize.Value;
+                    }
+                    else{
+                        t7.TextureMask.enabled = false;
+                    }
+                    
                     vintage.enabled = false;
-                    // no idea how to change the clip plane distance, YET.
+                    // Far/near clip plane distance is done on the optic_camera for each "mode" acording to UABEA,
+                    // just need to see where it is in the data structure
+                    return;
                 }
-                if (thermal.On && !t7.On) { // Make sure the thermal headgear isn't on, no need to do worthless changes.
+                else if (scope != null && thermal != null && (thermal.On || t7.InProcessSwitching)) {
+                    //scope.
                     thermal.IsGlitch = Plugin.ThermalGlitch.Value;
                     thermal.IsPixelated = Plugin.ThermalPixel.Value;
                     thermal.IsNoisy = Plugin.ThermalGlitch.Value;
@@ -84,6 +94,7 @@ namespace ClearVision {
                         thermal.StuckFpsUtilities.MinFramerate = Plugin.ThermalFpsMax.Value;
                     }
                     vintage.enabled = false;
+                    return;
                 }
                 else {
                     vintage.enabled = true;
--- ClientMod/Plugin.cs
+++ ClientMod/Plugin.cs
@@ -40,35 +40,33 @@ namespace ClearVision {
         private void Start() {
             Debug.LogError("ClearVision Start()");
             GlobalEnabled = Config.Bind("ClearVision", "GlobalEnabled", true, new ConfigDescription("Main Toggle", null));
-            // Night Vision Goggles
-            // GPNVG-18 Goggles
-            NVGEnabled = Config.Bind("GPNVG", "GPEnabled", true, new ConfigDescription("Enable GPNVG-18", null));
-            NVGIntensity = Config.Bind("GPNVG", "GPIntensity", 2.3f, new ConfigDescription("NVG Intensity", null));
-            NVGNoiseIntensity = Config.Bind("GPNVG", "GPNoiseIntensity", 0.0f, new ConfigDescription("NVG Noise Intensity", null));
-            NVGNoiseScale = Config.Bind("GPNVG", "GPNoiseScale", 0.0f, new ConfigDescription("NVG Noise Scale", null));
-            NVGDiffuseIntensity = Config.Bind("GPNVG", "GPDiffuseIntensity", 0.0f, new ConfigDescription("NVG Diffuse Intensity", null));
-            NVGMaskEnabled = Config.Bind("GPNVG", "GPMaskEnabled", true, new ConfigDescription("NVG Mask Overlay", null));
-            NVGMaskSize = Config.Bind("GPNVG", "GPMaskSize", 1.2f, new ConfigDescription("NVG Mask Overlay Size", null));
-            NVGColor = Config.Bind("GPNVG", "GPColor", new Color(0.00f, 0.78f, 0.78f, 0.99f), new ConfigDescription("NVG Color", null));
-            // Thermal Goggles/Scopes
+            // NVG Goggles
+            NVGEnabled = Config.Bind("NVG", "GPEnabled", true, new ConfigDescription("Enable GPNVG-18", null));
+            NVGIntensity = Config.Bind("NVG", "GPIntensity", 2.3f, new ConfigDescription("NVG Intensity", null));
+            NVGNoiseIntensity = Config.Bind("NVG", "GPNoiseIntensity", 0.0f, new ConfigDescription("NVG Noise Intensity", null));
+            NVGNoiseScale = Config.Bind("NVG", "GPNoiseScale", 0.0f, new ConfigDescription("NVG Noise Scale", null));
+            NVGDiffuseIntensity = Config.Bind("NVG", "GPDiffuseIntensity", 0.0f, new ConfigDescription("NVG Diffuse Intensity", null));
+            NVGMaskEnabled = Config.Bind("NVG", "GPMaskEnabled", true, new ConfigDescription("NVG Mask Overlay", null));
+            NVGMaskSize = Config.Bind("NVG", "GPMaskSize", 1.2f, new ConfigDescription("NVG Mask Overlay Size", null));
+            NVGColor = Config.Bind("NVG", "GPColor", new Color(0.00f, 0.78f, 0.78f, 0.99f), new ConfigDescription("NVG Color", null));
             // T-7 60Hz?
             T7Enabled = Config.Bind("T7", "T7Enabled", true, new ConfigDescription("Enable T-7", null));
-            T7Noise = Config.Bind("T7", "T7Noise", false, new ConfigDescription("Thermal Noise", null));
-            T7Glitch = Config.Bind("T7", "T7Glitch", false, new ConfigDescription("Thermal Glitchiness", null));
-            T7Pixel = Config.Bind("T7", "T7Pixel", false, new ConfigDescription("Thermal Pixelation", null));
-            T7MotionBlur = Config.Bind("T7", "T7MotionBlur", false, new ConfigDescription("Thermal Motion Blur", null));
-            T7Fps = Config.Bind("T7", "T7Fps", false, new ConfigDescription("Thermal FPS Lock Enabled", null));
-            T7FpsMax = Config.Bind("T7", "T7FpsMax", 60, new ConfigDescription("Thermal FPS Max FPS", new AcceptableValueRange<int>(15, 60)));
-            T7MaskEnabled = Config.Bind("T7", "T7MaskEnabled", true, new ConfigDescription("Thermal Mask Overlay", null));
-            T7MaskSize = Config.Bind("T7", "T7MaskSize", 1.5f, new ConfigDescription("Thermal Mask Overlay Size", new AcceptableValueRange<float>(0f, 5f)));
+            T7Noise = Config.Bind("T7", "T7Noise", false, new ConfigDescription("T-7 Thermal Noise", null));
+            T7Glitch = Config.Bind("T7", "T7Glitch", false, new ConfigDescription("T-7 Thermal Glitchiness", null));
+            T7Pixel = Config.Bind("T7", "T7Pixel", false, new ConfigDescription("T-7 Thermal Pixelation", null));
+            T7MotionBlur = Config.Bind("T7", "T7MotionBlur", false, new ConfigDescription("T-7 Thermal Motion Blur", null));
+            T7Fps = Config.Bind("T7", "T7Fps", false, new ConfigDescription("T-7 FPS Lock Enabled", null));
+            T7FpsMax = Config.Bind("T7", "T7FpsMax", 60, new ConfigDescription("T-7 FPS Max FPS", new AcceptableValueRange<int>(15, 60)));
+            T7MaskEnabled = Config.Bind("T7", "T7MaskEnabled", true, new ConfigDescription("T-7 Mask Overlay On/Off", null));
+            T7MaskSize = Config.Bind("T7", "T7MaskSize", 1.5f, new ConfigDescription("T-7 Mask Overlay Size", new AcceptableValueRange<float>(0f, 5f)));
             // Thermal 60Hz
-            ThermalEnabled = Config.Bind("Thermal", "ThermalEnabled", true, new ConfigDescription("Enable Thermal Sights", null));
-            ThermalNosie = Config.Bind("Thermal", "ThermalNosie", false, new ConfigDescription("Thermal Noise", null));
-            ThermalGlitch = Config.Bind("Thermal", "ThermalGlitch", false, new ConfigDescription("Thermal Glitchiness", null));
-            ThermalPixel = Config.Bind("Thermal", "ThermalPixel", false, new ConfigDescription("Thermal Pixelation", null));
-            ThermalMotionBlur = Config.Bind("Thermal", "ThermalMotionBlur", false, new ConfigDescription("Thermal Motion Blur", null));
-            ThermalFpsLock = Config.Bind("Thermal", "ThermalFpsLock", true, new ConfigDescription("Thermal FPS Lock Enabled", null));
-            ThermalFpsMax = Config.Bind("Thermal", "ThermalFpsMax", 60, new ConfigDescription("Thermal FPS Max FPS", new AcceptableValueRange<int>(15, 60)));
+            ThermalEnabled = Config.Bind("Scopes", "ThermalEnabled", true, new ConfigDescription("Enable Thermal Sights", null));
+            ThermalNosie = Config.Bind("Scopes", "ThermalNosie", false, new ConfigDescription("Thermal Noise", null));
+            ThermalGlitch = Config.Bind("Scopes", "ThermalGlitch", false, new ConfigDescription("Thermal Glitchiness", null));
+            ThermalPixel = Config.Bind("Scopes", "ThermalPixel", false, new ConfigDescription("Thermal Pixelation", null));
+            ThermalMotionBlur = Config.Bind("Scopes", "ThermalMotionBlur", false, new ConfigDescription("Thermal Motion Blur", null));
+            ThermalFpsLock = Config.Bind("Scopes", "ThermalFpsLock", true, new ConfigDescription("Thermal FPS Lock Enabled", null));
+            ThermalFpsMax = Config.Bind("Scopes", "ThermalFpsMax", 60, new ConfigDescription("Thermal FPS Max FPS", new AcceptableValueRange<int>(15, 60)));
         }
 
         private void Awake() {
